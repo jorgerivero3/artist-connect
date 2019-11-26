8c08730843692022f54893ac3a9bff27a1c1b11e
Jorge Rivero III <jrivero@Jorges-Air.attlocal.net>
2019-11-26 16:10:37 -0600

    Made bands and band page

---
+++ bands.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php
+		//TODO: Open database stuff 
+	?>
+	<!-- Bootstrap core CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	
+	<title>Artist Connect - Bands</title>
+	<meta charset="utf-8">
+</head>
+<body>
+
+</body>
+</html>

f9ef74a92fc44a0a10cc5c187cdc8d637736d394
Mason Mooring <mmooring@cs.utexas.edu>
2019-12-04 17:24:53 -0600

    new branch with updated database

---
+++ authenticate.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+// Change this to your connection info.
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'cs329e_mitra_mmooring';
+$DATABASE_PASS = 'banal5Fix3Soon';
+$DATABASE_NAME = 'cs329e_mitra_mmooring';
+// Try and connect using the info above.
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if ( mysqli_connect_errno() ) {
+	// If there is an error with the connection, stop the script and display the error.
+	die ('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+
+// Prepare statement to prevent injection
+if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
+	$stmt->bind_param('s', $_POST['username']);
+	$stmt->execute();
+	// Store the result so we can check if the account exists in the database.
+	$stmt->store_result();
+}
+
+if ($stmt->num_rows > 0) {
+	$stmt->bind_result($id, $password);
+	$stmt->fetch();
+	// Account exists, now we verify the password.
+	// Note: remember to use password_hash in your registration file to store the hashed passwords.
+	if ($_POST['password'] === $password) {
+		// Verification success! User has loggedin!
+		// Create sessions so we know the user is logged in, they basically act like cookies but remember the data on the server.
+		session_regenerate_id();
+		$_SESSION['loggedin'] = TRUE;
+		$_SESSION['name'] = $_POST['username'];
+		$_SESSION['id'] = $id;
+		echo 'Welcome ' . $_SESSION['name'] . '!';
+	} else {
+		echo 'Incorrect password!';
+	}
+} else {
+	echo 'Incorrect username!';
+}
+$stmt->close();
+?>

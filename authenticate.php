6f05a1b6a24e45d0a05c0833a6e1da0b53a0878a
Jorge <jrivero@utexas.edu>
2020-07-23 15:01:34 -0600

    Added postgres

--- authenticate.php
+++ authenticate.php
@@ -1,16 +1,33 @@
 <?php
 session_start();
-// Change this to your connection info.
+
+/* MYSQL Connction
 $DATABASE_HOST = 'ec2-52-86-116-94.compute-1.amazonaws.com';
 $DATABASE_USER = 'exkdsjszufcyiv';
 $DATABASE_PASS = 'b9b349ef1789507f99a76ea67d245e119c7f895f104cda9b772444efea26f929';
 $DATABASE_NAME = 'dblt6pefh8nddm';
+
+
+
+
 // Try and connect using the info above.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+
 if ( mysqli_connect_error() ) {
 	// If there is an error with the connection, stop the script and display the error.
 	die ('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
+*/
+
+//Postgress
+$con = pg_connect(getenv("DATABASE_URL"))
+
+if (!$con){
+	die('failed to connect to Postgres Server')
+}
+
+
+
 
 // Now we check if the data from the login form was submitted, isset() will check if the data exists.
 if ( !isset($_POST['username'], $_POST['password']) ) {
